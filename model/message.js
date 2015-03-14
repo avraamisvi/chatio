94a574815470eeb69601601b78062cc8d49a95e4
Abraao Isvi <abraao.isvi@gmail.com>
2015-03-14 19:17:43 -0300

    more dev

--- model/message.js
+++ model/message.js
@@ -3,7 +3,8 @@ var mongoose = require('mongoose');
 var MessageSchema = new mongoose.Schema({
   conversation: String,
   text: String,
-  sender: String
+  sender: String,
+  created: { type: Date, default: Date.now }
 });
 
 mongoose.model('Message', MessageSchema);
--- routes/messages.js
+++ routes/messages.js
@@ -1,19 +1,43 @@
 var express = require('express');
 var mongoose = require('mongoose');
+require('date-utils');
 
 var router = express.Router();
 
 router.get('/list', function(req, res, next) {
 
-  var Message = mongoose.model("Message");
+  var Message  = mongoose.model("Message");
+  var lastDay  = Date.now();
+  var firstDay = Date.today();
 
-  Message.where('conversation').equals(req.query.id).exec(function (err, resl) {
+  var lastDaynum = 0;
+  var firstDaynum = 2;
+
+  if(req.query.lastDay) {
+    lastDaynum = req.query.lastDay;
+  }
+
+  if(req.query.firstDay) {
+    firstDaynum = req.query.firstDay;
+  }
+
+  if(lastDaynum)
+    lastDay.removeDays(lastDaynum);
+
+  firstDay.removeDays(firstDaynum);
+
+  Message.where('conversation').
+  equals(req.query.id).
+  where('created').
+  gte(firstDay).
+  lte(lastDay).
+  exec(function (err, resl) {
 
     if (err) {
       console.error(err);
       return;
     }
-    
+
     var data = {messages:[]};
 
     if(resl)
